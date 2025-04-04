69a0d85d011646d9c3c2d8ea906d5b21ba2342bd
TarasPliushch <pliushch_ak23@nuwm.edu.ua>
2025-04-04 14:54:26 +0300

    Create App.vue

---
+++ App.vue
@@ -0,0 +1,14 @@
+// App.vue
+<script>
+export default {
+  // ... інші властивості
+  computed: {
+    activeTodos() {
+      return this.todos.filter(todo => !todo.completed)
+    },
+    completedTodos() {
+      return this.todos.filter(todo => todo.completed)
+    }
+  }
+}
+</script>
